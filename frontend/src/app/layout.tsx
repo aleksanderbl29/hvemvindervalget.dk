cee2ba4ab4cd8db2892674a73ff70e35e4848659
aleksanderbl29 <github@aleksanderbl.dk>
2025-11-21 11:39:28 +0100

    Add vercel analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { Providers } from "./providers";
@@ -71,6 +72,7 @@ export default function RootLayout({
             {children}
           </Theme>
         </Providers>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
